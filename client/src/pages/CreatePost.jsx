50ed2f670190c482bedada4be73e5a3ecd943a30
Saif-hadd <haddedseif746@gmail.com>
2025-09-03 00:49:25 +0200

    `Update API URLs to use Render.com endpoint`
    or
    
    `Replace local API URLs with Render.com endpoint`

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -38,7 +38,7 @@ const CreatePost = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://dall-e-eokx.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: form.prompt }),
@@ -67,7 +67,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-eokx.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
